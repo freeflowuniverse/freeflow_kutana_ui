cfef708785a6f334793cd60744c4ff3b92282e1b
coeneivan <ivan@ivancoene.be>
2020-03-26 10:40:52 +0100

    Send and receive images

--- src/components/TheChatInput.vue
+++ src/components/TheChatInput.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="theChatInput grey lighten-3">
     <v-divider class="py-2" horizontal></v-divider>
-    <v-text-field
-      v-model="message"
-      outlined
-      clearable
-      label="Message"
-      prepend-icon="attach_file"
-      append-icon="insert_emoticon"
-      append-outer-icon="send"
-      @click:append-outer="forwardMessage"
-    />
+    <v-form @submit.prevent="forwardMessage">
+      <input type="file" ref="fileUpper" @change="fileUploaded" style="display:none" />
+      <v-text-field
+        v-model="message"
+        outlined
+        clearable
+        label="Message"
+        prepend-icon="attach_file"
+        append-icon="insert_emoticon"
+        append-outer-icon="send"
+        @click:prepend="showFileUploader"
+        @click:append-outer="forwardMessage"
+      />
+    </v-form>
   </section>
 </template>
 
@@ -20,29 +24,67 @@ import moment from "moment";
 export default {
   data() {
     return {
+      file: null,
       message: ""
     };
   },
   mounted() {
-    // TODO: get chat
+    // window.addEventListener("paste", e => {
+    //   console.log(`PASTE`)
+    //   console.log(e)
+    //   this.$refs['fileUpper'].files = e.clipboardData.files;
+    // });
+    document.onpaste = (event) => {
+      let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+      console.log(JSON.stringify(items)); // will give you the mime types
+      for (let index in items) {
+        let item = items[index];
+        if (item.kind === "file") {
+          let blob = item.getAsFile();
+          let reader = new FileReader();
+          reader.onload = (event) => {
+            this.sendIt(event.target.result, 'file')
+          }; // data url!
+          reader.readAsDataURL(blob);
+        }
+      }
+    };
   },
   computed: {
-    ...mapGetters(['account'])
+    ...mapGetters(["account"])
   },
   methods: {
     ...mapActions(["sendMessage"]),
+    showFileUploader() {
+      this.$refs.fileUpper.click();
+    },
     forwardMessage() {
-      if (this.message !== "") {
+      this.sendIt(this.message)
+    },
+    clearMessage() {
+      this.message = "";
+    },
+    sendIt(content, type="text") {
+       if (content !== "") {
         this.sendMessage({
           sender: this.account.name,
           createdAt: moment(),
-          content: this.message
+          content,
+          type
         });
         this.clearMessage();
       }
     },
-    clearMessage() {
-      this.message = "";
+    async fileUploaded(e) {
+      this.sendIt(await this.toBase64(e.srcElement.files[0]), 'file')
+    },
+    toBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
     }
   }
 };
--- src/components/TheChatMessage.vue
+++ src/components/TheChatMessage.vue
@@ -1,11 +1,19 @@
 <template>
-  <v-card :class="message.sender === account.name ? 'ml-6 mr-2' : 'mr-12 ml-1'" elevation="0" color="primary" dark>
+  <v-card
+    :class="message.sender === account.name ? 'ml-6 mr-2' : 'mr-12 ml-1'"
+    elevation="0"
+    color="primary"
+    dark
+  >
     <v-card-subtitle>
       <v-row class="pr-3 pl-3">
         <span class="font-weight-bold">{{ message.sender }}</span>
       </v-row>
     </v-card-subtitle>
-    <v-card-text class="font-weight-medium">{{ message.content }}</v-card-text>
+    <v-card-text v-if="message.type === 'text'" class="font-weight-medium">{{ message.content }}</v-card-text>
+    <v-card-text v-if="mimeType === 'image/png'">
+      <v-img :src="message.content"></v-img>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -16,7 +24,13 @@ export default {
   computed:{
     ...mapGetters([
       'account'
-    ])
+    ]),
+    mimeType()  {
+      if(this.message.type === "file") {
+        return this.message.content.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0]
+      } 
+      return false
+    }
   }
 };
 </script>
