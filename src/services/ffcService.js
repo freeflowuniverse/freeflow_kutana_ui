7062badcb753a7cfc312ddced6fe91b12132be79
Mathias <mathias@pop-os.localdomain>
2020-04-03 13:27:47 +0200

    added slash

--- src/services/ffcService.js
+++ src/services/ffcService.js
@@ -4,6 +4,6 @@ import config from '../../public/config'
 export default ({
   getTeamInfo(teamName = 'test') {
       console.log(`${config.ffcBackend}api/teams/${teamName}`)
-    return Axios.get(`${config.ffcBackend}api/teams/${teamName}`)
+    return Axios.get(`${config.ffcBackend}api/teams/${teamName}/`)
   }
 })
