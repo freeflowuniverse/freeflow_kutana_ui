4582f7a8a6281ff770fb849736e6c81aa35a1726
Mathias <mathias@pop-os.localdomain>
2020-04-08 13:36:44 +0200

    removed setRoomId

--- src/views/WaitingRoom.vue
+++ src/views/WaitingRoom.vue
@@ -31,14 +31,4 @@ export default {
     }
   }
 };
-
-
-function hashString(str){
-	let hash = 0;
-	for (let i = 0; i < str.length; i++) {
-		hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
-		hash = hash & hash; // Convert to 32bit integer
-	}
-	return hash;
-}
 </script>
