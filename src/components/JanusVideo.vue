1c05922f1aeea7bbfad2236e67657c519ac29476
Mathias <mathias@pop-os.localdomain>
2020-04-15 12:32:28 +0200

    progress

--- src/components/JanusVideo.vue
+++ src/components/JanusVideo.vue
@@ -1,15 +1,15 @@
 <template>
-  <AspectRatio ar="4:3">
+  <!-- <AspectRatio ar="4:3"> -->
     <video :src-object.prop.camel="stream" autoplay="autoplay" :muted="muted"></video>
-  </AspectRatio>
+  <!-- </AspectRatio> -->
 </template>
 
 <script type="javascript">
-import AspectRatio from "../components/AspectRatio.vue";
+// import AspectRatio from "../components/AspectRatio";
 
 export default {
   components: {
-    AspectRatio
+    // AspectRatio
   },
   props: {
     stream: {
@@ -29,11 +29,8 @@ export default {
 
 <style lang="scss" scoped>
 video {
-  width: 100%;
   height: 100%;
-
   max-width: 100%;
-  max-height: 100%;
-  object-fit: fill;
+  object-fit: contain;
 }
 </style>
