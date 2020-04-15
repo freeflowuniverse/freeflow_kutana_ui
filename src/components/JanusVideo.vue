93cecf48d71c2f21ee0fc739e5147ee0415b8605
Mathias <mathias@pop-os.localdomain>
2020-04-15 11:33:58 +0200

    Added vue aspect ratio instead of own imple.

--- src/components/JanusVideo.vue
+++ src/components/JanusVideo.vue
@@ -2,19 +2,21 @@
   <div
     :class="this.$props.stream.getVideoTracks().length ? 'video-present janus-video' : 'video-not-present janus-video'"
   >
-    <video :src-object.prop.camel="stream" autoplay="autoplay" :muted="muted"></video>
-    <v-row align="center" justify="center" class="video-cam-off">
-      <v-icon color="white">videocam_off</v-icon>
-    </v-row>
+    <AspectRatio ar="4:3">
+      <video :src-object.prop.camel="stream" autoplay="autoplay" :muted="muted"></video>
+      <v-row align="center" justify="center" class="video-cam-off">
+        <v-icon color="white">videocam_off</v-icon>
+      </v-row>
+    </AspectRatio>
   </div>
 </template>
 
 <script type="javascript">
-// import AspectRatio from "../components/AspectRatio.vue";
+import AspectRatio from "../components/AspectRatio.vue";
 
 export default {
   components: {
-    // AspectRatio
+    AspectRatio
   },
   props: {
     stream: {
@@ -33,25 +35,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.janus-video {
-  position: relative;
-  flex: 1;
-}
 
-.janus-video:before {
-  display: block;
-  content: "";
-  width: 100%;
-  padding-top: (3/4) * 100%;
-}
-
-.janus-video video,
-.janus-video .row {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
+video {
   width: 100%;
   height: 100%;
   object-fit: fill;
