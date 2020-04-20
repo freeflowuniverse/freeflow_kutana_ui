33c0a85007e7b3f2504cec08d42da604b25467cb
Mathias <mathias@pop-os.localdomain>
2020-04-20 12:43:24 +0200

    janus02

--- public/config.js
+++ public/config.js
@@ -6,5 +6,5 @@ export default {
     botFrontEnd: "https://login.staging.jimber.org/",
     botBackend: "https://login.staging.jimber.org/",
     debugJanus: false,
-    janusServer: "https://janus.staging.jimber.org/janus"
+    janusServer: "https://janus02.staging.jimber.org/janus"
 }
