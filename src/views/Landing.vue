99f7709a6ef039db390f094b8a67e7b7f0d1a433
Jonas Wijne <jonas.wijne@jimber.org>
2020-06-04 11:50:40 +0200

    refactor file

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,88 +1,89 @@
 <template>
-  <v-row align="center" justify="center" class="landing px-5">
-    <v-col cols="12" md="6">
-      <v-card>
-        <v-card-title>
-          <p class="text-center" style="width:100%">FreeFlowConnect</p>
-        </v-card-title>
-        <v-card-text>
-          <v-row align="center" justify="space-around">
-            <v-col cols="12" md="5" align="center">
-              <v-form @submit.prevent="joinRoom" v-model="valid">
-                <v-text-field
-                  filled
-                  label="Invite link or room ID"
-                  persistent-hint
-                  v-model="inviteUrl"
-                  hint="Paste the link or room ID you've received"
-                  :rules="inviteUrlRules"
-                  required
-                >
-                  <template v-slot:append>
-                    <v-btn :disabled="!valid" small text type="submit">Join room</v-btn>
-                  </template>
-                </v-text-field>
-              </v-form>
-            </v-col>
-            <v-divider :vertical="$vuetify.breakpoint.mdAndUp"></v-divider>
-            <v-col cols="12" md="5" align="center">
-              <v-btn text @click="create">Create room</v-btn>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row align="center" justify="center" class="landing px-5">
+        <v-col cols="12" md="6">
+            <v-card>
+                <v-card-title>
+                    <h1 class="text-center" style="font-size:1.25rem;width:100%">FreeFlowConnect</h1>
+                </v-card-title>
+                <v-card-text>
+                    <v-row align="center" justify="space-around">
+                        <v-col cols="12" md="5" align="center">
+                            <v-form @submit.prevent="joinRoom" v-model="valid">
+                                <v-text-field
+                                        filled
+                                        label="Invite link or room ID"
+                                        persistent-hint
+                                        v-model="inviteUrl"
+                                        hint="Paste the link or room ID you've received"
+                                        :rules="inviteUrlRules"
+                                        required
+                                >
+                                    <template v-slot:append>
+                                        <v-btn :disabled="!valid" small text type="submit">Join room</v-btn>
+                                    </template>
+                                </v-text-field>
+                            </v-form>
+                        </v-col>
+                        <v-divider :vertical="$vuetify.breakpoint.mdAndUp"></v-divider>
+                        <v-col cols="12" md="5" align="center">
+                            <v-btn text @click="create">Create room</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 <script>
-import { mapActions, mapGetters } from "vuex";
-export default {
-  data() {
-    return {
-      /* eslint-disable */
-      reg: new RegExp("(?:https://.*/room/)?(.*)"),
-      /* eslint-enable */
-      valid: false,
-      inviteUrlRules: [
-        url => !!url || "Invite url is required",
-        url => this.reg.test(url) || "Invite url or room ID  invalid"
-      ],
-      inviteUrl: null
+    import {mapActions, mapGetters} from "vuex";
+
+    export default {
+        data() {
+            return {
+                /* eslint-disable */
+                reg: new RegExp("(?:https://.*/room/)?(.*)"),
+                /* eslint-enable */
+                valid: false,
+                inviteUrlRules: [
+                    url => !!url || "Invite url is required",
+                    url => this.reg.test(url) || "Invite url or room ID  invalid"
+                ],
+                inviteUrl: null
+            };
+        },
+        computed: {
+            ...mapGetters(["account", "teamName"])
+        },
+        methods: {
+            ...mapActions(["createTeam", "join"]),
+            create() {
+                this.createTeam();
+            },
+            joinRoom() {
+                if (this.inviteUrl && this.reg.test(this.inviteUrl)) {
+                    this.$router.push({
+                        name: "room",
+                        params: {token: this.inviteUrl.match(this.reg)[1].substring(0, 15)}
+                    });
+                }
+            }
+        },
+        watch: {
+            inviteUrl(val) {
+                if (val && this.reg.test(val) && val.length > 15) {
+                    this.inviteUrl = val.match(this.reg)[1];
+                }
+            },
+            teamName(val) {
+                if (val) {
+                    this.$router.push({name: "room", params: {token: val}});
+                }
+            }
+        }
     };
-  },
-  computed: {
-    ...mapGetters(["account", "teamName"])
-  },
-  methods: {
-    ...mapActions(["createTeam", "join"]),
-    create() {
-      this.createTeam();
-    },
-    joinRoom() {
-      if (this.inviteUrl && this.reg.test(this.inviteUrl)) {
-        this.$router.push({
-          name: "room",
-          params: { token: this.inviteUrl.match(this.reg)[1].substring(0, 15) }
-        });
-      }
-    }
-  },
-  watch: {
-    inviteUrl(val) {
-      if (val && this.reg.test(val) && val.length > 15) {
-        this.inviteUrl = val.match(this.reg)[1];
-      }
-    },
-    teamName(val) {
-      if (val) {
-        this.$router.push({ name: "room", params: { token: val } });
-      }
-    }
-  }
-};
 </script>
 <style lang="scss" scoped>
-.landing {
-  background: #f5f5f5;
-}
+    .landing {
+        background: #f5f5f5;
+    }
 </style>
