aa770532120fb763550fdf7f21d70504f84b33b4
Jonas Wijne <jonas.wijne@jimber.org>
2020-06-30 11:18:18 +0200

    add device change

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,18 +2,36 @@
     <div class="settings">
         <v-dialog v-model="show">
             <v-card>
-                <v-toolbar dark color="primary">
-                    <v-btn icon dark @click="dialog = false">
+                <v-toolbar color="primary" dark>
+                    <v-btn @click="dialog = false" dark icon>
                         <v-icon>close</v-icon>
                     </v-btn>
                     <v-toolbar-title>Settings</v-toolbar-title>
                 </v-toolbar>
-                <v-list three-line subheader>
+                <v-list subheader three-line>
                     <v-subheader>Device controls</v-subheader>
                     <v-list-item>
                         <v-list-item-content>
                             <v-list-item-title>Input Video Device</v-list-item-title>
-                            <v-select></v-select>
+                            <v-select
+                                    :items="videoDevices"
+                                    item-text="label"
+                                    item-value="deviceId"
+                                    @change="changeVideoDevice"
+                                    :value="selectedVideo"
+                            ></v-select>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-title>Input Audio Device</v-list-item-title>
+                            <v-select
+                                    :items="audioDevices"
+                                    item-text="label"
+                                    item-value="deviceId"
+                                    @change="changeAudioDevice"
+                                    :value="selectedAudio"
+                            ></v-select>
                         </v-list-item-content>
                     </v-list-item>
                 </v-list>
@@ -26,16 +44,52 @@
     export default {
         name: 'Settings',
         props: {
-            value: Boolean
+            value: Boolean,
         },
         computed: {
             show: {
-                get () {
-                    return this.value
+                get() {
+                    return this.value;
                 },
-                set (value) {
-                    this.$emit('input', value)
-                }
+                set(value) {
+                    this.$emit('input', value);
+                },
+            },
+        },
+        data: function() {
+            return {
+                videoDevices: [],
+                audioDevices: [],
+                selectedVideo: null,
+                selectedAudio: null
+            };
+        },
+        mounted() {
+            navigator.mediaDevices.enumerateDevices().then(devices => {
+                this.videoDevices = devices.filter(d => d.kind === 'videoinput');
+                this.audioDevices = devices.filter(d => d.kind === 'audioinput');
+                this.selectedVideo = devices.find(d => d.label === window.janusshizzle.videoRoomPlugin.myStream.getVideoTracks()[0]?.label)?.deviceId
+                this.selectedAudio = devices.find(d => d.label === window.janusshizzle.videoRoomPlugin.myStream.getAudioTracks()[0]?.label)?.deviceId
+            });
+        },
+        methods: {
+            async changeVideoDevice(value) {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: {
+                        deviceId: value
+                    }
+                })
+
+                await window.janusshizzle.videoRoomPlugin.publishTrack(stream.getVideoTracks()[0])
+            },
+            async changeAudioDevice(value) {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    audio: {
+                        deviceId: value
+                    }
+                })
+
+                await window.janusshizzle.videoRoomPlugin.publishTrack(stream.getAudioTracks()[0])
             }
         }
     };
