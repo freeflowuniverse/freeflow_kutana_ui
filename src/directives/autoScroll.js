efded7db0fc6efb4589eb19a89590a03c6a3b63e
Ivan Coene <ivan@ivancoene.be>
2020-07-07 13:21:56 +0200

    Scroll chat to bottom on create

--- src/directives/autoScroll.js
+++ src/directives/autoScroll.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 
 export default {
+    inserted: el => {
+        Vue.nextTick().then(() => {
+            el.scrollTop = el.scrollHeight
+        })
+    },
     update: el => {
         Vue.nextTick().then(() => {
             el.scrollTop = el.scrollHeight
