12ffd3c8711f225ec997a4a31f2e787c1c22fe3d
Ivan Coene <ivan@ivancoene.be>
2020-07-15 15:34:07 +0200

    Open chat after clicking on notification

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -8,7 +8,7 @@
         class="room"
         v-if="allUsers.length && allScreenUsers.length"
     >
-<div class="test">{{test}}</div>
+        <div class="test">{{test}}</div>
         <UserGrid :users="users" :showChat="view === 'chat'">
             <template v-slot:chat>
                 <ChatGrid
@@ -41,233 +41,224 @@
             ></audio>
         </div>
         <Settings v-model="showSettings"></Settings>
-        <ChatMessageNotification class="notifications" v-if="view != 'chat'" />
+        <ChatMessageNotification
+            class="notifications"
+            v-if="view != 'chat'"
+            @click="view =  view === 'chat' ? 'grid' : 'chat'"
+        />
     </div>
 </template>
 
 <script>
-    import { mapActions, mapGetters, mapMutations } from 'vuex';
-    import UserGrid from '../components/UserGrid';
-    import ChatGrid from '../components/ChatGrid';
-    import ControlStrip from '../components/ControlStrip';
-    import { initializeJanus } from '../services/JanusService';
-    import config from '../../public/config';
-    import router from '../plugins/router';
-    import store from '../plugins/vuex';
-    import { v4 as uuidv4 } from 'uuid';
-    import { reject } from 'lodash/collection';
-    import { isNull } from 'lodash/lang';
-    import Settings from '../components/Settings';
-    import ChatMessageNotification from '../components/ChatMessageNotification';
+import { mapActions, mapGetters, mapMutations } from 'vuex';
+import UserGrid from '../components/UserGrid';
+import ChatGrid from '../components/ChatGrid';
+import ControlStrip from '../components/ControlStrip';
+import { initializeJanus } from '../services/JanusService';
+import config from '../../public/config';
+import router from '../plugins/router';
+import store from '../plugins/vuex';
+import { v4 as uuidv4 } from 'uuid';
+import { reject } from 'lodash/collection';
+import { isNull } from 'lodash/lang';
+import Settings from '../components/Settings';
+import ChatMessageNotification from '../components/ChatMessageNotification';
 
-    export default {
-        name: 'Room',
-        components: {
-            Settings,
-            UserGrid,
-            ControlStrip,
-            ChatGrid,
-            ChatMessageNotification
-        },
-        data() {
-            return {
-                isGrid: true,
-                showUserList: true,
-                startX: null,
-                dragging: false,
-                view: 'grid',
-                showControls: false,
-                timeout: null,
-                showSettings: false,
-            };
-        },
-        beforeMount() {
-            if (!store.getters.localStream) {
-                router.push({
+export default {
+    name: 'Room',
+    components: {
+        Settings,
+        UserGrid,
+        ControlStrip,
+        ChatGrid,
+        ChatMessageNotification,
+    },
+    data() {
+        return {
+            isGrid: true,
+            showUserList: true,
+            startX: null,
+            dragging: false,
+            view: 'grid',
+            showControls: false,
+            timeout: null,
+            showSettings: false,
+        };
+    },
+    beforeMount() {
+        if (!store.getters.localStream) {
+            router.push({
+                name: 'joinRoom',
+                params: { token: this.$route.params.token },
+            });
+            return;
+        }
+
+        this.isGrid = this.isGridView;
+        this.join(this.$route.params.token);
+        this.getTeamInfo();
+    },
+    async mounted() {
+        if (!store.getters.localStream) {
+            try {
+                await router.push({
                     name: 'joinRoom',
                     params: { token: this.$route.params.token },
                 });
-                return;
-            }
+            } catch (e) {}
 
-            this.isGrid = this.isGridView;
-            this.join(this.$route.params.token);
-            this.getTeamInfo();
+            return;
+        }
+
+        if (this.localUser) {
+            return;
+        }
+        //@todo get from prejoin room
+        // const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const stream = store.getters.localStream;
+        store.commit('setLocalStream', null);
+
+        //@todo fixme
+        const userName =
+            this.account.name ||
+            localStorage.getItem('account').name ||
+            `test-${Math.random()
+                .toString(36)
+                .replace(/[^a-z]+/g, '')
+                .substr(0, 5)}`;
+
+        const roomName = this.hashString(this.$route.params.token);
+        const opaqueId = uuidv4();
+        const userControl = await initializeJanus(
+            config.janusServer,
+            opaqueId,
+            `${uuidv4()}-${userName}`,
+            roomName,
+            stream
+        );
+        this.setUserControl(userControl);
+    },
+    methods: {
+        ...mapActions([
+            'setSnackbarMessage',
+            'getTeamInfo',
+            'join',
+            'changeViewStyle',
+        ]),
+        ...mapMutations(['setUserControl']),
+        hashString(str) {
+            let hash = 0;
+            for (let i = 0; i < str.length; i++) {
+                hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
+                hash = hash & hash;
+            }
+            return Math.abs(hash);
         },
-        async mounted() {
-            if (!store.getters.localStream) {
-                try {
-                    await router.push({
-                        name: 'joinRoom',
-                        params: { token: this.$route.params.token },
+        copyUrl() {
+            navigator.clipboard
+                .writeText(this.inviteLink)
+                .then(() => {
+                    this.setSnackbarMessage({
+                        type: '',
+                        text: `Link copied to clipboard`,
                     });
-                } catch (e) {}
-
-                return;
-            }
-            this.$root.$on('toggleGridPresentation', () => {
-                this.changeViewStyle(this.isGrid ? 'Default' : 'Grid');
-                this.isGrid = !this.isGrid;
-            });
-
-            this.$root.$on('toggleSidebar', () => {
-                this.showSidebar = !this.showSidebar;
-            });
-
-            this.$root.$on('toggleUserList', () => {
-                this.showUserList = !this.showUserList;
-            });
-
-            if (this.localUser) {
-                return;
-            }
-            //@todo get from prejoin room
-            // const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-            const stream = store.getters.localStream;
-            store.commit('setLocalStream', null);
-
-            //@todo fixme
-            const userName =
-                this.account.name ||
-                localStorage.getItem('account').name ||
-                `test-${Math.random()
-                    .toString(36)
-                    .replace(/[^a-z]+/g, '')
-                    .substr(0, 5)}`;
-
-            const roomName = this.hashString(this.$route.params.token);
-            const opaqueId = uuidv4();
-            const userControl = await initializeJanus(
-                config.janusServer,
-                opaqueId,
-                `${uuidv4()}-${userName}`,
-                roomName,
-                stream
+                })
+                .catch(e => {
+                    console.error(e);
+                });
+        },
+        startDrag(e) {
+            e.disable;
+            this.startX = e.clientX;
+            this.startWidth = parseInt(
+                document.defaultView.getComputedStyle(this.$refs.sidebar).width,
+                10
             );
-            this.setUserControl(userControl);
+            document.addEventListener('mousemove', this.doDrag, false);
+            document.addEventListener('mouseup', this.stopDrag, false);
         },
-        methods: {
-            ...mapActions([
-                'setSnackbarMessage',
-                'getTeamInfo',
-                'join',
-                'changeViewStyle',
-            ]),
-            ...mapMutations(['setUserControl']),
-            hashString(str) {
-                let hash = 0;
-                for (let i = 0; i < str.length; i++) {
-                    hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
-                    hash = hash & hash;
-                }
-                return Math.abs(hash);
-            },
-            copyUrl() {
-                navigator.clipboard
-                    .writeText(this.inviteLink)
-                    .then(() => {
-                        this.setSnackbarMessage({
-                            type: '',
-                            text: `Link copied to clipboard`,
+        stopDrag() {
+            document.removeEventListener('mousemove', this.doDrag, false);
+            document.removeEventListener('mouseup', this.stopDrag, false);
+        },
+        doDrag(e) {
+            console.log(e);
+            this.$refs.sidebar.style.width =
+                this.startWidth + this.startX - e.clientX + 'px';
+        },
+        showControl() {
+            this.showControls = true;
+            clearTimeout(this.timeout);
+            this.timeout = window.setTimeout(() => {
+                this.showControls = false;
+            }, 4000);
+        },
+    },
+    computed: {
+        ...mapGetters([
+            'remoteUsers',
+            'teamName',
+            'isGridView',
+            'localUser',
+            'allUsers',
+            'allScreenUsers',
+            'isMobile',
+            'account',
+            'userControl',
+        ]),
+        test() {
+            return this.$refs.controlstrip;
+        },
+        inviteLink() {
+            let baseUrl = window.location.href;
+            if (baseUrl.charAt(baseUrl.length - 1) !== '/') {
+                baseUrl += '/';
+            }
+            return `${baseUrl}`;
+        },
+        users() {
+            if (!(this.allUsers.length && this.allScreenUsers.length)) {
+                return [];
+            }
+            return reject(
+                this.allUsers
+                    .map(u => {
+                        const screenUser = this.allScreenUsers.find(su => {
+                            return su.uuid === u.uuid;
                         });
+
+                        if (!screenUser) {
+                            return null;
+                        }
+
+                        return {
+                            ...u,
+                            screenShareStream: screenUser.stream,
+                            screen: screenUser.screen,
+                        };
                     })
-                    .catch(e => {
-                        console.error(e);
-                    });
-            },
-            startDrag(e) {
-                e.disable;
-                this.startX = e.clientX;
-                this.startWidth = parseInt(
-                    document.defaultView.getComputedStyle(this.$refs.sidebar)
-                        .width,
-                    10
-                );
-                document.addEventListener('mousemove', this.doDrag, false);
-                document.addEventListener('mouseup', this.stopDrag, false);
-            },
-            stopDrag() {
-                document.removeEventListener('mousemove', this.doDrag, false);
-                document.removeEventListener('mouseup', this.stopDrag, false);
-            },
-            doDrag(e) {
-                console.log(e);
-                this.$refs.sidebar.style.width =
-                    this.startWidth + this.startX - e.clientX + 'px';
-            },
-            showControl() {
-                this.showControls = true;
-                clearTimeout(this.timeout);
-                this.timeout = window.setTimeout(() => {
-                    this.showControls = false;
-                }, 4000);
-            },
+                    .reverse(),
+                isNull
+            );
         },
-        computed: {
-            ...mapGetters([
-                'remoteUsers',
-                'teamName',
-                'isGridView',
-                'localUser',
-                'allUsers',
-                'allScreenUsers',
-                'isMobile',
-                'account',
-                'userControl',
-            ]),
-            test () {
-                return this.$refs.controlstrip
-            },
-            inviteLink() {
-                let baseUrl = window.location.href;
-                if (baseUrl.charAt(baseUrl.length - 1) !== '/') {
-                    baseUrl += '/';
-                }
-                return `${baseUrl}`;
-            },
-            users() {
-                if (!(this.allUsers.length && this.allScreenUsers.length)) {
-                    return [];
-                }
-                return reject(
-                    this.allUsers
-                        .map(u => {
-                            const screenUser = this.allScreenUsers.find(su => {
-                                return su.uuid === u.uuid;
-                            });
-
-                            if (!screenUser) {
-                                return null;
-                            }
-
-                            return {
-                                ...u,
-                                screenShareStream: screenUser.stream,
-                                screen: screenUser.screen,
-                            };
-                        })
-                        .reverse(),
-                    isNull
-                );
-            },
-        },
-    };
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-    .room {
-        height: calc(var(--vh) * 100);
-        .test {
-            top: 0;
-            width: 100%;
-            position: fixed;
-            z-index: 2;
-        }
-        .notifications {
-            position: fixed;
-            bottom: 0px;
-            right: 0;
-            z-index: 2;
-        }
+.room {
+    height: calc(var(--vh) * 100);
+    .test {
+        top: 0;
+        width: 100%;
+        position: fixed;
+        z-index: 2;
     }
+    .notifications {
+        position: fixed;
+        bottom: 0px;
+        right: 0;
+        z-index: 2;
+    }
+}
 </style>
