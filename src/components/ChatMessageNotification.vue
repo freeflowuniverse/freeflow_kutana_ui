c38186b28c0d95906629c3f0ea85c1f4c0911a44
Ivan Coene <ivan@ivancoene.be>
2020-07-15 15:33:47 +0200

    Only showing 3 last notifications && deleting it after 1s

--- src/components/ChatMessageNotification.vue
+++ src/components/ChatMessageNotification.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="notifications">
+    <section class="notifications" @click="$emit('click')">
         <div class="notification px-5 py-2 white ma-5" v-for="message in messagesToShow" :key="message.createdAt">
             <!-- TODO crop content -->
             <span class="from secondary--text font-weight-bold">{{message.sender}}</span>: {{message.content}} 
@@ -19,15 +19,21 @@ export default {
     },
     watch: {
         messages(val) {
-            // TODO check if chat is vissible
             // TODO check if overlapping with controls
             const lastMessage = val[val.length -1]
             if (lastMessage.sender != this.account.name) {
+                lastMessage.timeout = setTimeout(() => {
+                    this.messagesToShow = this.messagesToShow.filter(m => m != lastMessage)
+                }, 1000)
                 this.messagesToShow.push(lastMessage)
-            }
-            if (val.length > 3) {
+            } 
+            if (this.messagesToShow.length > 3) {
+                this.messagesToShow.slice(0, this.messagesToShow.length -3).forEach(message => {
+                    clearTimeout(message.timeout)
+                });             
                 this.messagesToShow = this.messagesToShow.slice(-3)
             }
+            
         },
     },
 };
@@ -38,5 +44,6 @@ export default {
     border-radius: 5px;
     box-shadow: 0px 0px 6px rgba(255, 255, 255, 0.48);
     border-radius: 6px;
+    cursor: pointer;
 }
 </style>
