1ec5aac946ed6743c18685aef3ad7ebddf88faad
Dean Verjans <developer.dean@outlook.com>
2020-08-10 17:00:46 +0200

    Fix button state of cam and mic & Add tooltips to buttons

--- src/components/ControlStrip.vue
+++ src/components/ControlStrip.vue
@@ -1,26 +1,61 @@
 <template>
     <v-row justify="center" ref="controlstrip" class="mb-5" style="z-index: 99999999999999999">
-        <v-btn @click="toggleCam" class="primary mx-2" dark icon :fab="!isMobile" :loading="isCamLoading">
-            <v-icon :small="isMobile">{{ localUser.cam ? 'videocam_off' : 'videocam' }}</v-icon>
-        </v-btn>
-        <v-btn @click="toggleMic" class="primary mx-2" dark icon :fab="!isMobile" :loading="isMicLoading">
-            <v-icon :small="isMobile">{{ localUser.mic ? 'mic_off' : 'mic' }}</v-icon>
-        </v-btn>
-        <v-btn @click="screen" class="primary mx-2" dark icon :fab="!isMobile" v-if="!isMobile">
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="toggleCam" class="primary mx-2" dark icon v-bind="attrs" v-on="on" :fab="!isMobile" :loading="isCamLoading">
+            <v-icon :small="isMobile">{{ localUser.cam ? 'videocam' : 'videocam_off' }}</v-icon>
+          </v-btn>
+        </template>
+        <span>Turn {{ localUser.cam ? 'Off' : 'On' }} Camera</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="toggleMic" class="primary mx-2" v-bind="attrs" v-on="on" dark icon :fab="!isMobile" :loading="isMicLoading">
+            <v-icon :small="isMobile">{{ localUser.mic ? 'mic' : 'mic_off' }}</v-icon>
+          </v-btn>
+        </template>
+        <span>{{ localUser.mic ? 'Mute' : 'Unmute' }} Microphone</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="screen" class="primary mx-2" v-bind="attrs" v-on="on" dark icon :fab="!isMobile" v-if="!isMobile">
             <v-icon :small="isMobile">{{ localScreenUser.screen ? 'stop_screen_share' : 'screen_share' }}</v-icon>
-        </v-btn>
-        <v-btn @click="hangUp" class="red mx-2" dark icon :fab="!isMobile">
+          </v-btn>
+        </template>
+        <span>{{ localScreenUser.screen ? 'Stop' : 'Start' }} Screenshare</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="hangUp" class="red mx-2" v-bind="attrs" v-on="on" dark icon :fab="!isMobile">
             <v-icon :small="isMobile">call_end</v-icon>
-        </v-btn>
-        <v-btn @click="$emit('toggleChat')" class="primary mx-2" dark icon :fab="!isMobile">
+          </v-btn>
+        </template>
+        <span>Disconnect</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="$emit('toggleChat')" class="primary mx-2" v-bind="attrs" v-on="on" dark icon :fab="!isMobile">
             <v-icon :small="isMobile">chat_bubble</v-icon>
-        </v-btn>
-        <v-btn disabled class="primary mx-2" dark icon :fab="!isMobile">
+          </v-btn>
+        </template>
+        <span>Open Chat</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn disabled class="primary mx-2" v-bind="attrs" v-on="on" dark icon :fab="!isMobile">
             <v-icon :small="isMobile">broken_image</v-icon>
-        </v-btn>
-        <v-btn @click="$emit('openSettings', $event)" class="btn-settings primary mx-2" dark icon :fab="!isMobile">
+          </v-btn>
+        </template>
+        <span>Toggle Background</span>
+      </v-tooltip>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="$emit('openSettings', $event)" class="btn-settings primary mx-2" v-bind="attrs" v-on="on"  dark icon :fab="!isMobile">
             <v-icon :small="isMobile">settings</v-icon>
-        </v-btn>
+          </v-btn>
+        </template>
+        <span>Open Settings</span>
+      </v-tooltip>
     </v-row>
 </template>
 <script>
@@ -39,7 +74,7 @@ export default {
             'localUser',
             'localScreenUser',
             'isMobile',
-            'videoDeviceId'
+            'videoDeviceId',
         ]),
         micEnabled() {
             return true;
