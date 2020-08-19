c6f373e49a4c9ad104e2f01b221c09e5f3bd9ef7
Jonas Wijne <jonas.wijne@jimber.org>
2020-08-19 15:12:32 +0200

    prettier App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,108 @@
 <template>
     <v-app :style="cssProps">
         <router-view></router-view>
-        <v-snackbar
-            top
-            v-model="showSnackbar"
-            :color="snackbarMessage.type"
-        >{{snackbarMessage.text}}</v-snackbar>
+        <v-snackbar :color="snackbarMessage.type" top v-model="showSnackbar"
+            >{{ snackbarMessage.text }}
+        </v-snackbar>
     </v-app>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-import { initMediaDeviceDetection } from './utils/mediaDevicesUtils';
+    import { mapGetters } from 'vuex';
+    import { initMediaDeviceDetection } from './utils/mediaDevicesUtils';
 
-export default {
-    data() {
-        return {
-            showSnackbar: false,
-        };
-    },
-    computed: {
-        ...mapGetters(['snackbarMessage']),
-        cssProps() {
+    export default {
+        data() {
             return {
-                '--primary-color': this.$vuetify.theme.themes.light.primary,
-                '--accent-color': this.$vuetify.theme.themes.light.accent,
-                '--error-color': this.$vuetify.theme.themes.light.error,
+                showSnackbar: false,
             };
         },
-    },
-    mounted() {
-        initMediaDeviceDetection();
-        let vh = window.innerHeight * 0.01;
-
-        // Then we set the value in the --vh custom property to the root of the document
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-        window.addEventListener('resize', () => {
+        computed: {
+            ...mapGetters(['snackbarMessage']),
+            cssProps() {
+                return {
+                    '--primary-color': this.$vuetify.theme.themes.light.primary,
+                    '--accent-color': this.$vuetify.theme.themes.light.accent,
+                    '--error-color': this.$vuetify.theme.themes.light.error,
+                };
+            },
+        },
+        mounted() {
+            initMediaDeviceDetection();
             let vh = window.innerHeight * 0.01;
+
             // Then we set the value in the --vh custom property to the root of the document
             document.documentElement.style.setProperty('--vh', `${vh}px`);
-        });
-    },
-    watch: {
-        snackbarMessage(val) {
-            if (val) {
-                this.showSnackbar = true;
-            }
-            setTimeout(() => {
-                this.snackbar = false;
-            }, 6000);
+            window.addEventListener('resize', () => {
+                let vh = window.innerHeight * 0.01;
+                // Then we set the value in the --vh custom property to the root of the document
+                document.documentElement.style.setProperty('--vh', `${vh}px`);
+            });
         },
-    },
-};
+        watch: {
+            snackbarMessage(val) {
+                if (val) {
+                    this.showSnackbar = true;
+                }
+                setTimeout(() => {
+                    this.snackbar = false;
+                }, 6000);
+            },
+        },
+    };
 </script>
 <style lang="scss">
-.v-application .title.ttl,
-.ttl {
-    font-family: 'Bebas Neue', cursive !important;
-}
-.chatMessage {
-    .content p {
-        margin-bottom: 0;
+    .v-application .title.ttl,
+    .ttl {
+        font-family: 'Bebas Neue', cursive !important;
     }
-    &.mine a {
-        color: white;
+
+    .chatMessage {
+        .content p {
+            margin-bottom: 0;
+        }
+
+        &.mine a {
+            color: white;
+        }
     }
-}
-.video-main video.noScreenshare {
-    transform: rotateY(180deg);
-}
-.mobile-room-grid {
-    #selectedStream {
-        background: white;
+
+    .video-main video.noScreenshare {
+        transform: rotateY(180deg);
     }
-}
-.mine video,
-video.mine {
-    transform: rotateY(180deg);
-}
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 300ms;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0;
-}
-.shrink-x-enter-active,
-.shrink-x-leave-active {
-    transition: max-width 300ms;
-    max-width: 100% !important;
-    overflow: hidden !important;
-}
-.shrink-x-enter,
-.shrink-x-leave-to {
-    max-width: 0 !important;
-    overflow: hidden !important;
-}
-.v-btn-toggle--rounded {
-    overflow: hidden;
-}
+
+    .mobile-room-grid {
+        #selectedStream {
+            background: white;
+        }
+    }
+
+    .mine video,
+    video.mine {
+        transform: rotateY(180deg);
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 300ms;
+    }
+
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+ {
+        opacity: 0;
+    }
+
+    .shrink-x-enter-active,
+    .shrink-x-leave-active {
+        transition: max-width 300ms;
+        max-width: 100% !important;
+        overflow: hidden !important;
+    }
+    .shrink-x-enter,
+    .shrink-x-leave-to {
+        max-width: 0 !important;
+        overflow: hidden !important;
+    }
+    .v-btn-toggle--rounded {
+        overflow: hidden;
+    }
 </style>
