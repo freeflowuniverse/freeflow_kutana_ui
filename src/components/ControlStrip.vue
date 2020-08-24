5756babb0e2ce7ff21a1050eb390a8ea3c71580c
Ivan Coene <ivan@ivancoene.be>
2020-08-24 15:32:47 +0200

    Fix #134 changing devices not working

--- src/components/ControlStrip.vue
+++ src/components/ControlStrip.vue
@@ -157,7 +157,7 @@ export default {
         },
     },
     methods: {
-        ...mapActions(['getAudioStream', 'getVideoStream']),
+        ...mapActions(['getAudioStream', 'getVideoStream', 'updateAudioDevice', 'updateVideoDevice']),
         ...mapMutations([
             'setLocalUser',
             'toggleAudioActive',
