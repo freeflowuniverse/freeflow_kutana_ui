51da02fa0c53cda9e06f131dd2dfcd0425b0fa98
Dean Verjans <developer.dean@outlook.com>
2020-09-01 15:03:07 +0200

    Add auto join view

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import Landing from '../views/Landing.vue';
 import Room from '../views/Room.vue';
 import Dev from '../views/Dev.vue';
+import AutoJoin from '../views/AutoJoin';
 
 Vue.use(VueRouter);
 
@@ -12,6 +13,7 @@ export default new VueRouter({
     routes: [
         { path: '/', name: 'home', component: Landing, meta: { requiresAuth: false } },
         { path: '/dev', name: 'dev', component: Dev, meta: { requiresAuth: false } },
+        { path: '/autoJoin', name: 'autoJoin', component: AutoJoin, meta: { requiresAuth: false } },
         { path: '/room/:token', name: 'room', component: Room, meta: { requiresAuth: true, hideAccount: true } },
     ],
 });
