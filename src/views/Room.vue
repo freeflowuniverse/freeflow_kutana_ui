975266828350231ce91c852029380fef36401b71
Ivan Coene <ivan@ivancoene.be>
2020-09-03 13:57:59 +0200

    Moving the buttons z-index up a bit so it's clickable when dialog is open

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -8,24 +8,26 @@
         class="room"
         v-if="allUsers.length && allScreenUsers.length"
     >
-        <div v-if="(remoteUsers.length <= 0 && showInvitation) || forceOpenInvitation" :class="showInvitation || forceOpenInvitation ? 'invite': ''">
-          <InviteUsers @closeInvitations="closeInvitations" />
-        </div>
-        <UserGrid :users="users" :showChat="view === 'chat'" :view="currentViewStyle">
+        <v-dialog v-model="showInvitation">
+            <InviteUsers @closeInvitations="closeInvitations" />
+        </v-dialog>
+        <UserGrid :users="users" :showChat="view === 'chat'">
             <template v-slot:chat>
                 <ChatGrid
                     v-if="view === 'chat'"
                     :selectedUser="localUser"
-                    v-on:back="view = 'no-chat'"
+                    v-on:back="view = 'grid'"
                 ></ChatGrid>
             </template>
             <template v-slot:controlStrip>
                 <v-row justify="center" class="controlStrip mx-0">
                     <ControlStrip
                         class="mx-0"
-                        @toggleChat="view === 'chat' ? (view = 'no-chat') : (view = 'chat')"
+                        @toggleChat="
+                            view === 'chat' ? (view = 'grid') : (view = 'chat')
+                        "
                         @openSettings="showSettings = true"
-                        @openInvitations="forceOpenInvitation = true"
+                        @openInvitations="showInvitation = true"
                     ></ControlStrip>
                 </v-row>
             </template>
@@ -40,204 +42,192 @@
                 v-for="user of remoteUsers"
             ></audio>
         </div>
-        <Settings v-if="userControl" v-model="showSettings" @change-view="changeViewStyle"></Settings>
+        <Settings v-if="userControl" v-model="showSettings"></Settings>
         <ChatMessageNotification
             class="notifications"
             v-if="view !== 'chat'"
-            @click="view = view === 'chat' ? 'no-chat' : 'chat'"
+            @click="view = view === 'chat' ? 'grid' : 'chat'"
         />
     </div>
 </template>
 
 <script>
-    import { mapActions, mapGetters, mapMutations } from 'vuex';
-    import UserGrid from '../components/UserGrid';
-    import ChatGrid from '../components/ChatGrid';
-    import ControlStrip from '../components/ControlStrip';
-    import { initializeJanus } from '../services/JanusService';
-    import config from '../../public/config';
-    import router from '../plugins/router';
-    import store from '../plugins/vuex';
-    import { v4 as uuidv4 } from 'uuid';
-    import { reject } from 'lodash/collection';
-    import { isNull } from 'lodash/lang';
-    import Settings from '../components/Settings';
-    import ChatMessageNotification from '../components/ChatMessageNotification';
-    import { uniqBy } from 'lodash/array';
-    import InviteUsers from '../components/InviteUsers';
+import { mapActions, mapGetters, mapMutations } from 'vuex';
+import UserGrid from '../components/UserGrid';
+import ChatGrid from '../components/ChatGrid';
+import ControlStrip from '../components/ControlStrip';
+import { initializeJanus } from '../services/JanusService';
+import config from '../../public/config';
+import router from '../plugins/router';
+import store from '../plugins/vuex';
+import { v4 as uuidv4 } from 'uuid';
+import { reject } from 'lodash/collection';
+import { isNull } from 'lodash/lang';
+import Settings from '../components/Settings';
+import ChatMessageNotification from '../components/ChatMessageNotification';
+import { uniqBy } from 'lodash/array';
+import InviteUsers from '../components/InviteUsers';
 
-    export default {
-        name: 'Room',
-        components: {
-          InviteUsers,
-            Settings,
-            UserGrid,
-            ControlStrip,
-            ChatGrid,
-            ChatMessageNotification,
-        },
-        data() {
-            return {
-                view: 'no-chat',
-                showControls: false,
-                timeout: null,
-                showSettings: false,
-                showInvitation: true,
-                forceOpenInvitation: false
-            };
-        },
-        async mounted() {
-            if (!this.localStream) {
-              try {
-                await router.push({
-                  name: 'home',
-                  query: { roomName: this.$route.params.token },
-                });
-              } catch (e) {}
-              return;
-            }
-
-            this.join(this.$route.params.token);
-            this.getTeamInfo();
-
-            //@todo get from prejoin room
-            // const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-            const stream = this.localStream;
-            store.commit('setLocalStream', null);
-
-            //@todo fixme
-            const userName =
-                this.account.name ||
-                `test-${Math.random()
-                    .toString(36)
-                    .replace(/[^a-z]+/g, '')
-                    .substr(0, 5)}`;
-
-            const roomName = this.hashString(this.$route.params.token);
-            const opaqueId = uuidv4();
-            const userUuid = this.account.uuid || uuidv4();
-            const userControl = await initializeJanus(
-                config.janusServer,
-                opaqueId,
-                `${userUuid}-${userName}`,
-                roomName,
-                stream
-            );
-            this.setUserControl(userControl);
-
-            if ('Notification' in window) {
-                if (Notification.permission !== 'denied') {
-                    Notification.requestPermission();
-                }
-            }
-        },
-        methods: {
-            ...mapActions([
-                'setSnackbarMessage',
-                'getTeamInfo',
-                'join',
-                'changeViewStyle',
-                'setPresenterMode'
-            ]),
-            ...mapMutations([
-                'setUserControl',
-                'setPresentationMessage'
-            ]),
-            hashString(str) {
-                let hash = 0;
-                for (let i = 0; i < str.length; i++) {
-                    hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
-                    hash = hash & hash;
-                }
-                return Math.abs(hash);
-            },
-            showControl() {
-                this.showControls = true;
-                clearTimeout(this.timeout);
-                this.timeout = window.setTimeout(() => {
-                    this.showControls = false;
-                }, 4000);
-            },
-            closeInvitations() {
-              this.showInvitation = false;
-              this.forceOpenInvitation = false;
-            }
-        },
-        computed: {
-            ...mapGetters([
-                'remoteUsers',
-                'teamName',
-                'viewStyle',
-                'localUser',
-                'allUsers',
-                'allScreenUsers',
-                'isMobile',
-                'account',
-                'userControl',
-                'presentationMessage',
-                'localStream',
-                'presenter'
-            ]),
-            currentViewStyle: {
-              get() {
-                return this.presenter ? 'presentation' : this.viewStyle;
-              }
-            },
-            users() {
-                if (!(this.allUsers.length && this.allScreenUsers.length)) {
-                    return [];
-                }
-                const users = reject(
-                    this.allUsers.map(u => {
-                        const screenUser = this.allScreenUsers.find(su => {
-                            return su.uuid === u.uuid;
-                        });
-
-                        if (!screenUser) {
-                            return null;
-                        }
-
-                        return {
-                            ...u,
-                            screenShareStream: screenUser.stream,
-                            screen: screenUser.screen,
-                            presenter: this.presenter
-                        };
-                    }),
-                    isNull
-                );
-                return uniqBy(users, 'uuid').reverse();
-            },
-        },
-        watch: {
-          localUser(val) {
-            if (!val) {
-              return;
-            }
-            if (this.presentationMessage) {
-              this.setPresenterMode();
-              this.setPresentationMessage(null);
-            }
-          }
+export default {
+    name: 'Room',
+    components: {
+        InviteUsers,
+        Settings,
+        UserGrid,
+        ControlStrip,
+        ChatGrid,
+        ChatMessageNotification,
+    },
+    data() {
+        return {
+            isGrid: true,
+            showUserList: true,
+            startX: null,
+            dragging: false,
+            view: 'grid',
+            showControls: false,
+            timeout: null,
+            showSettings: false,
+            showInvitation: true,
+        };
+    },
+    beforeMount() {
+        if (!store.getters.localStream) {
+            router.push({
+                name: 'home',
+                params: { token: this.$route.params.token },
+            });
+            return;
         }
-    };
+
+        this.isGrid = this.isGridView;
+        this.join(this.$route.params.token);
+        this.getTeamInfo();
+    },
+    async mounted() {
+        if (!store.getters.localStream) {
+            try {
+                await router.push({
+                    name: 'home',
+                    query: { roomName: this.$route.params.token },
+                });
+            } catch (e) {}
+
+            return;
+        }
+
+        if (this.localUser) {
+            return;
+        }
+        //@todo get from prejoin room
+        // const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        const stream = store.getters.localStream;
+        store.commit('setLocalStream', null);
+
+        //@todo fixme
+        const userName =
+            this.account.name ||
+            `test-${Math.random()
+                .toString(36)
+                .replace(/[^a-z]+/g, '')
+                .substr(0, 5)}`;
+
+        const roomName = this.hashString(this.$route.params.token);
+        const opaqueId = uuidv4();
+        const userUuid = this.account.uuid || uuidv4();
+        const userControl = await initializeJanus(
+            config.janusServer,
+            opaqueId,
+            `${userUuid}-${userName}`,
+            roomName,
+            stream
+        );
+        this.setUserControl(userControl);
+
+        if ('Notification' in window) {
+            if (Notification.permission !== 'denied') {
+                Notification.requestPermission();
+            }
+        }
+    },
+    methods: {
+        ...mapActions([
+            'setSnackbarMessage',
+            'getTeamInfo',
+            'join',
+            'changeViewStyle',
+        ]),
+        ...mapMutations(['setUserControl']),
+        hashString(str) {
+            let hash = 0;
+            for (let i = 0; i < str.length; i++) {
+                hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
+                hash = hash & hash;
+            }
+            return Math.abs(hash);
+        },
+        showControl() {
+            this.showControls = true;
+            clearTimeout(this.timeout);
+            this.timeout = window.setTimeout(() => {
+                this.showControls = false;
+            }, 4000);
+        },
+        closeInvitations() {
+            this.showInvitation = false;
+        },
+    },
+    computed: {
+        ...mapGetters([
+            'remoteUsers',
+            'teamName',
+            'isGridView',
+            'localUser',
+            'allUsers',
+            'allScreenUsers',
+            'isMobile',
+            'account',
+            'userControl',
+        ]),
+        users() {
+            if (!(this.allUsers.length && this.allScreenUsers.length)) {
+                return [];
+            }
+            const users = reject(
+                this.allUsers.map(u => {
+                    const screenUser = this.allScreenUsers.find(su => {
+                        return su.uuid === u.uuid;
+                    });
+
+                    if (!screenUser) {
+                        return null;
+                    }
+
+                    return {
+                        ...u,
+                        screenShareStream: screenUser.stream,
+                        screen: screenUser.screen,
+                    };
+                }),
+                isNull
+            );
+            return uniqBy(users, 'uuid').reverse();
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-    .room {
-        height: calc(var(--vh) * 100);
-        .notifications {
-            position: fixed;
-            bottom: 0px;
-            right: 0;
-            z-index: 2;
-        }
+.room {
+    height: calc(var(--vh) * 100);
+    .notifications {
+        position: fixed;
+        bottom: 0px;
+        right: 0;
+        z-index: 2;
     }
-    .invite {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      background: rgb(0, 0, 0, 0.5);
-      z-index: 2;
-    }
+}
+.controlStrip {
+    z-index: 213;
+}
 </style>
