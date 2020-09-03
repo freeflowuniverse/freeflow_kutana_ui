64a5e3ac0da1eece38b9eb19166c1ae2326bad77
Ivan Coene <ivan@ivancoene.be>
2020-09-03 13:09:53 +0200

    #157 removing file type in message (not implemented yet)

--- src/components/TheChatMessage.vue
+++ src/components/TheChatMessage.vue
@@ -24,29 +24,6 @@
       <v-card-text v-if="message.type === 'text'" class="font-weight-bold content pa-1 pl-2">
         <div v-dompurify-html="parseMarkdown"></div>
       </v-card-text>
-      <v-card-text v-else-if="message.type === 'file' && mimeType === 'image/png'">
-        <v-img :src="message.content.file"></v-img>
-      </v-card-text>
-      <v-card-text v-else-if="message.type === 'file' ">
-        <v-sheet color="primary lighten-2">
-          <v-row class="mx-0 pa-3" align="center">
-            <v-icon large>attachment</v-icon>
-            <v-col>
-              <p class="my-0">
-                <a
-                  :href="message.content.file"
-                  :download="message.content.name"
-                  class="white--text"
-                >{{message.content.name}}</a>
-              </p>
-              <p class="my-0 overline">{{fileSize}}</p>
-            </v-col>
-            <v-btn text icon :href="message.content.file" :download="message.content.name">
-              <v-icon>get_app</v-icon>
-            </v-btn>
-          </v-row>
-        </v-sheet>
-      </v-card-text>
     </v-card>
     <div class="text-center grey--text font-weight-light overline" v-else>{{message.content}}</div>
   </section>
@@ -67,24 +44,6 @@ export default {
   },
   computed: {
     ...mapGetters(["account"]),
-    mimeType() {
-      if (this.message.type === "file") {
-        return this.message.content.file.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0];
-      }
-      return false;
-    },
-    fileSize() {
-      if (this.message.type === "file") {
-        // The magic numbers from this function come from https://stackoverflow.com/a/49750491/2349421
-        let file = this.message.content.file;
-        let lengthOfHeader = file.indexOf("base64,") + "base64,".length;
-
-        let stringLength = file.length - lengthOfHeader;
-        let sizeInBytes = 4 * Math.ceil(stringLength / 3) * 0.5624896334383812;
-        return `${Math.round((sizeInBytes / 1000) * 100) / 100} KB`;
-      }
-      return false;
-    },
     parseMarkdown() {
       return marked(this.message.content);
     },
@@ -93,12 +52,6 @@ export default {
     }
   },
   methods: {
-    openFile() {
-      var win = window.open();
-      win.document.write(
-        `<iframe src="${this.message.content.file}" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>`
-      );
-    },
     displayTime() {
       this.showTime = true;
     }
