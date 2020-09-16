652dbc3c0b9d7dc5d61635af6094198947ddd6e6
Ivan Coene <ivan@ivancoene.be>
2020-09-16 13:06:36 +0200

    Fixing merge

--- src/components/ScreenShareSelector.vue
+++ src/components/ScreenShareSelector.vue
@@ -1,22 +1,12 @@
 <template>
-  <v-btn-toggle rounded class="primary mr-1" dark>
-    <v-tooltip top>
-      <template v-slot:activator="{ on: tooltip }">
-        <v-btn small class="primary" @click="$emit('toggle')" v-on="{ ...tooltip}">
-          <v-icon small>{{ isActive ? 'stop_screen_share' : 'screen_share' }}</v-icon>
-        </v-btn>
-      </template>
-      <span>{{ isActive ? 'Stop' : 'Start' }} screen sharing</span>
-    </v-tooltip>
-    <v-menu top left offset-y v-if="isActive">
-      <template v-slot:activator="{ on: menu }">
+    <v-btn-toggle rounded class="primary mr-1" dark>
         <v-tooltip top>
             <template v-slot:activator="{ on: tooltip }">
                 <v-btn class="primary" @click="$emit('toggle')" v-on="{ ...tooltip}">
                     <v-icon>{{ isActive ? 'stop_screen_share' : 'screen_share' }}</v-icon>
                 </v-btn>
             </template>
-            <span>{{ isActive ? 'Start' : 'stop' }} Screen sharing</span>
+            <span>{{ isActive ? 'Stop' : 'Start' }} screen sharing</span>
         </v-tooltip>
         <v-menu top left offset-y v-if="isActive">
             <template v-slot:activator="{ on: menu }">
