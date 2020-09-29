8f0383557852cf208bf84d6b5d098f5f62d3c162
Jonas Wijne <jonas.wijne@jimber.org>
2020-09-29 14:21:08 +0200

    :wrench: update prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    ignorePatterns: ['**/tests/**/*.js'],
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    rules: {
+        'no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                args: 'after-used',
+                ignoreRestSiblings: false,
+            },
+        ],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+};
