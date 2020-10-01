8a120ca8706cedd4f86c2066834a1d93186cb389
tobiaschielens <tobias.chielens@gmail.org>
2020-10-01 13:06:31 +0200

    added grid conf

--- public/config.grid.js
+++ public/config.grid.js
@@ -1,10 +1,10 @@
 export default {
-    ffcBackend: 'http://localhost:5000/',
+    ffcBackend: `${window.location.host}:5000/`,
     appId: window.location.host,
     scope: JSON.stringify({ doubleName: true }),
     redirect_url: `?callback=true`,
-    botFrontEnd: 'https://login.staging.jimber.org/',
-    botBackend: 'https://login.staging.jimber.org/',
+    botFrontEnd: 'https://login.threefold.me/',
+    botBackend: 'https://login.threefold.me/',
     debugJanus: false,
     janusServer: `${window.location.host}/janus`,
     gaId: 'UA-150836145-1',
