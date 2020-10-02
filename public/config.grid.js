2029cd847c1bea9b7a9e1b8bd4f4e65ce81c84b7
tobiaschielens <tobias.chielens@gmail.org>
2020-10-02 14:39:13 +0200

    fixing urls

--- public/config.grid.js
+++ public/config.grid.js
@@ -1,11 +1,11 @@
 export default {
-    ffcBackend: `http://${window.location.hostname}/`,
+    ffcBackend: `${window.location.protocol}//${window.location.hostname}/`,
     appId: window.location.host,
     scope: JSON.stringify({ doubleName: true }),
     redirect_url: `?callback=true`,
     botFrontEnd: 'https://login.threefold.me/',
     botBackend: 'https://login.threefold.me/',
     debugJanus: false,
-    janusServer: `http://${window.location.hostname}/janus`,
+    janusServer: `${window.location.protocol}//${window.location.hostname}/janus`,
     gaId: 'UA-150836145-1',
 };
