77187127d9dd6fa90ae744a95eee453011828846
tobiaschielens <tobias.chielens@gmail.org>
2020-10-02 11:43:16 +0200

    http backend

--- public/config.grid.js
+++ public/config.grid.js
@@ -1,11 +1,11 @@
 export default {
-    ffcBackend: `https://${window.location.hostname}/`,
+    ffcBackend: `http://${window.location.hostname}/`,
     appId: window.location.host,
     scope: JSON.stringify({ doubleName: true }),
     redirect_url: `?callback=true`,
     botFrontEnd: 'https://login.threefold.me/',
     botBackend: 'https://login.threefold.me/',
     debugJanus: false,
-    janusServer: `https://${window.location.hostname}/janus`,
+    janusServer: `http://${window.location.hostname}/janus`,
     gaId: 'UA-150836145-1',
 };
